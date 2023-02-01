e12992a18bc1a8d1f5e49a32ed2abaf5eacb63a1
desireevharvey <desireevharvey@gmail.com>
2023-02-01 16:00:28 -0500

    made changes ready to deploy

--- appTwo.js
+++ appTwo.js
@@ -6,3 +6,20 @@ function openOne() {
   modalOne.style.display = 'block'
 }
 modalOneBtn.addEventListener('click', openOne)
+
+
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+  modal[0].style.display = "none";
+  }
+  
+  span[1].onclick = function() {
+  modal[1].style.display = "none";
+  }
+      
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+  }
